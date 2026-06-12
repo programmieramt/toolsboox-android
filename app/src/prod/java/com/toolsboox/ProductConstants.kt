6b7742bf0c239911548868e023bc17a898d91c75
Sebastian <sebastian@programmieramt.de>
2026-06-12 06:55:17 +0200

    refactor: remove unused SERVICE_BASE_URL from ProductConstants

--- app/src/prod/java/com/toolsboox/ProductConstants.kt
+++ app/src/prod/java/com/toolsboox/ProductConstants.kt
@@ -6,10 +6,5 @@ package com.toolsboox
  * @author <a href="mailto:gabor.auth@toolsboox.com">Gábor AUTH</a>
  */
 class ProductConstants {
-    companion object {
-        /**
-         * Base URL of the service.
-         */
-        const val SERVICE_BASE_URL = "https://api.toolsboox.com/"
-    }
+    companion object
 }
